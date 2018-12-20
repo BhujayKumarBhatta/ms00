a88ce6247807c9da6c1135583da84244e80f5502
94788 <94788@ITSSKOLINFRL031.ITC.IN>
2018-12-20 18:56:46 +0530

    Syntax error was in last file due to formating trying to rectify by manual typing in eclips

--- jenkins.sh
+++ jenkins.sh
@@ -1,10 +1,12 @@
-#cd /root/tokenleader-build;source /root/venvp3/bin/activate;/root/venvp3/bin/python3  -m unittest discover /root/tokenleader-build/tests
-rm -rf ~/venvp3
+DIR=~/venvp3
+if [ -d "$DIR" ]; then
+	printf '%s\n' "Removing venv old ($DIR)"
+	rm -rf "$DIR"
+fi
 virtualenv -p python3 ~/venvp3
-#root/venvp3/bin/python3 ~/venvp3/bin/activate
 source ~/venvp3/bin/activate
 pip install --upgrade pip
+echo -e "\n\n\n" | ssh-keygen -t rsa
 pip install -r ~/tokenleader-pipe/requirement.txt
-#cd /root/tokenleader-pipe;/root/venvp3/bin/python3  -m unittest discover /root/tokenleader-pipe/tests
 cd ~/tokenleader-pipe
-python3  -m unittest discover tests
+python3 -m unittest discover tests
