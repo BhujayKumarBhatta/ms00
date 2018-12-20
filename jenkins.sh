6f60a38227ab82b059354eb443bcf641be89e4b1
94788 <94788@ITSSKOLINFRL031.ITC.IN>
2018-12-20 18:48:49 +0530

    last sh did not work

--- jenkins.sh
+++ jenkins.sh
@@ -1,12 +1,10 @@
-DIR=~/venvp3
-if [ -d "$DIR" ]; then
-	printf '%s\n' "Removing venv ($DIR)"
-	rm -rf "$DIR"
-fi
+#cd /root/tokenleader-build;source /root/venvp3/bin/activate;/root/venvp3/bin/python3  -m unittest discover /root/tokenleader-build/tests
+rm -rf ~/venvp3
 virtualenv -p python3 ~/venvp3
+#root/venvp3/bin/python3 ~/venvp3/bin/activate
 source ~/venvp3/bin/activate
 pip install --upgrade pip
-echo -e "\n\n\n" | ssh-keygen -t rsa
 pip install -r ~/tokenleader-pipe/requirement.txt
+#cd /root/tokenleader-pipe;/root/venvp3/bin/python3  -m unittest discover /root/tokenleader-pipe/tests
 cd ~/tokenleader-pipe
 python3  -m unittest discover tests
