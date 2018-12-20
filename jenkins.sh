e33efd534f268bb5ad709ee926ed2d59ece1f5d7
94788 <94788@ITSSKOLINFRL031.ITC.IN>
2018-12-20 18:27:00 +0530

    File format changed due to copy paste from DOS to unix issue. trying to fix it

--- jenkins.sh
+++ jenkins.sh
@@ -3,11 +3,14 @@ if [ -d "$DIR" ]; then
     printf '%s\n' "Removing venv ($DIR)"
     rm -rf "$DIR"
 fi
-
+#rm -rf ~/venvp3
 virtualenv -p python3 ~/venvp3
+#root/venvp3/bin/python3 ~/venvp3/bin/activate
+
 source ~/venvp3/bin/activate
 pip install --upgrade pip
 echo -e "\n\n\n" | ssh-keygen -t rsa
 pip install -r ~/tokenleader-pipe/requirement.txt
+#cd /root/tokenleader-pipe;/root/venvp3/bin/python3  -m unittest discover /root/tokenleader-pipe/tests
 cd ~/tokenleader-pipe
 python3  -m unittest discover tests
