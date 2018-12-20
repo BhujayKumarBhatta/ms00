653ac01454b29ee606c2fc495d7890e9024c18e7
94788 <94788@ITSSKOLINFRL031.ITC.IN>
2018-12-20 18:39:28 +0530

    new jenkins

---
+++ jenkins.sh
@@ -0,0 +1,12 @@
+DIR=~/venvp3
+if [ -d "$DIR" ]; then
+	printf '%s\n' "Removing venv ($DIR)"
+	rm -rf "$DIR"
+fi
+virtualenv -p python3 ~/venvp3
+source ~/venvp3/bin/activate
+pip install --upgrade pip
+echo -e "\n\n\n" | ssh-keygen -t rsa
+pip install -r ~/tokenleader-pipe/requirement.txt
+cd ~/tokenleader-pipe
+python3  -m unittest discover tests
