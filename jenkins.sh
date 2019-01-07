607757440409e5143066149a71274b68a25dcf00
94788 <94788@ITSSKOLINFRL031.ITC.IN>
2019-01-07 16:02:31 +0530

    updated jenkins to understand build and test in better manner

--- jenkins.sh
+++ jenkins.sh
@@ -1,3 +1,4 @@
+echo "****Remove old virtual environment and old files****"
 DIR=~/venvp3
 if [ -d "$DIR" ]; then
 	printf '%s\n' "Removing venv old ($DIR)"
@@ -5,6 +6,7 @@ if [ -d "$DIR" ]; then
 fi
 chown jenkins /tmp/*.db
 rm -f /tmp/auth.db
+echo "****Create and run Virtual environment****"
 virtualenv -p python3 ~/venvp3
 source ~/venvp3/bin/activate
 pip install --upgrade pip
@@ -12,12 +14,18 @@ echo -e "\n\n\n" | ssh-keygen -t rsa
 pip install -r ~/tokenleader-pipe/requirement.txt
 cd ~/tokenleader-pipe
 rm -rf migrations
+echo "****Run unit test****"
 python3 -m unittest discover tests
 export FLASK_APP=app_run.py
+echo "****Migrate DB****"
 flask db init
 flask db migrate -m "DB Migration"
 flask db upgrade
+echo "****Run Token Service****"
 nohup flask run --port=9898 & 
 export FLASK_APP=app_register_login_user.py
+echo "****Run CURD Service****"
 nohup flask run --port=9999 &
+sleep 30
+echo "****Run UI test****"
 python3 tokenuitest.py
