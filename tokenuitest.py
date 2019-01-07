5f38f4908e20491a591e27b2c6ac138e4a6f44cc
94788 <94788@ITSSKOLINFRL031.ITC.IN>
2019-01-07 17:08:53 +0530

    tokenuitest.py: modified

--- tokenuitest.py
+++ tokenuitest.py
@@ -1,9 +1,12 @@
 from subprocess import check_output, check_call
 try:
-        a=check_output(["sudo", "curl", "-i", "-H", "Content-Type: application/json", "-X", "POST", "-d", '{"username":"admin","emailid":"admin@itc.in","role":"admin","password":"welcome@123"}', "http://localhost:9999/register_user"])
-        if 'HTTP/1.1 200 OK' in str(a):
-             b = check_output(["sudo", "curl", "-i", "-H", "Content-Type: application/json", "-X", "GET", "-d", '{"username":"admin","password":"welcome@123"}', "http://localhost:9999/login_user"])
-             if 'admin' in str(b):
+        a=check_output(["sudo", "curl", "-i", "-H", "Content-Type: application/json", "-X", "POST", "-d", '{"username":"admin4","emailid":"admin4@itc.in","role":"admin$
+        print(a)
+        if "Registration Successful" in a:
+             print('ok')
+             b = check_output(["sudo", "curl", "-i", "-H", "Content-Type: application/json", "-X", "GET", "-d", '{"username":"admin4","password":"welcome@123"}', "http$
+             print(b)
+             if 'admin' in b:
                     print('Admin User')
              else:
                     print('Non-admin User')
