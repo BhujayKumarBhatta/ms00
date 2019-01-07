e07d02a5cd0a08b983359520123aad94094fb8a2
94788 <94788@ITSSKOLINFRL031.ITC.IN>
2019-01-07 17:35:40 +0530

    str error

--- tokenuitest.py
+++ tokenuitest.py
@@ -1,12 +1,12 @@
 from subprocess import check_output, check_call
 try:
-        a=check_output(["sudo", "curl", "-i", "-H", "Content-Type: application/json", "-X", "POST", "-d", '{"username":"admin4","emailid":"admin4@itc.in","role":"admin","password":"welcome@1"}' "http://localhost:9999/register_user"])
+        a=check_output(["sudo", "curl", "-i", "-H", "Content-Type: application/json", "-X", "POST", "-d", '{"username":"admin3","emailid":"admin3@itc.in","role":"admin","password":"welcome@1"}', "http://localhost:9999/register_user"])
         print(a)
-        if "Registration Successful" in a:
+        if 'Registration Successful' in str(a):
              print('ok')
-             b = check_output(["sudo", "curl", "-i", "-H", "Content-Type: application/json", "-X", "GET", "-d", '{"username":"admin4","password":"welcome@1"}', "http://localhost:9999/login_user"]
+             b = check_output(["sudo", "curl", "-i", "-H", "Content-Type: application/json", "-X", "GET", "-d", '{"username":"admin3","password":"welcome@1"}', "http://localhost:9999/login_user"])
              print(b)
-             if 'admin' in b:
+             if 'admin' in str(b):
                     print('Admin User')
              else:
                     print('Non-admin User')
