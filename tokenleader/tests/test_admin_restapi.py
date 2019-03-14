b9b4ab2e2e151caf2a1ab4987948ef6c7bd472fb
project <project@project-PC>
2019-03-14 12:36:46 +0530

    y

--- tokenleader/tests/test_admin_restapi.py
+++ tokenleader/tests/test_admin_restapi.py
@@ -79,7 +79,7 @@ class TestAdminRestApi(BaseTestCase):
             self.assertTrue(data['status'] == 'u1 has been  deleted successfully')
             
     def test_delete_org_restapi(self):
-        u3 = t.create_org_for_test()
+        u1 = t.create_org_for_test()
         with self.client:
             response = self.client.delete('/delete/org/org1')
             data = json.loads(response.data.decode())
