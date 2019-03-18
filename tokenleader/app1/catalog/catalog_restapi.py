56e03d63efeb159340ed9ba250929d621f4f3985
project <project@project-PC>
2019-03-18 15:43:56 +0530

    n

---
+++ tokenleader/app1/catalog/catalog_restapi.py
@@ -0,0 +1,26 @@
+from flask import request, Blueprint, jsonify, current_app,make_response
+from tokenleader.app1.catalog import catalog_functions as cf , catalog_bp
+from tokenleaderclient.configs.config_handler import Configs    
+from tokenleaderclient.client.client import Client 
+from tokenleaderclient.rbac.enforcer import Enforcer
+from tokenleader.app1.adminops.adminops_restapi import adminops_bp
+
+
+auth_config = Configs()
+tlclient = Client(auth_config)
+enforcer = Enforcer(tlclient)
+ 
+   
+#@catalog_bp.route('/list/addservice/<servicename>/<pwd>/<urlint>/<urlext>/<urladmin>', methods=['GET'])
+#@enforcer.enforce_access_rule_with_token('tokenleader.adminops.adminops_restapi.list_users')
+#def add_service(wfc,servicename,pwd,urlint,urlext,urladmin):    
+    
+#    msg = cf.add_service(servicename, pwd, urlint, urlext, urladmin )     
+#    return msg
+
+@catalog_bp.route('/list/services', methods=['GET'])
+def list_services():    
+     
+    record_list = cf.list_services()
+    response_obj = {"status": record_list}
+    return jsonify(response_obj)
