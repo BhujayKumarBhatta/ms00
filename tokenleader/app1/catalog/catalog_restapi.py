c1e479c0d21078679b98555e5732c3dd45b935ac
project <project@project-PC>
2019-03-20 19:16:02 +0530

    h

--- tokenleader/app1/catalog/catalog_restapi.py
+++ tokenleader/app1/catalog/catalog_restapi.py
@@ -19,7 +19,7 @@ enforcer = Enforcer(tlclient)
 
 @catalog_bp.route('/list/services', methods=['GET'])
 #@enforcer.enforce_access_rule_with_token('tokenleader.adminops.adminops_restapi.list_services')
-def list_services(self):
+def list_service(self):
     services_dict = cf.list_service()
     obj_json = {"name": services_dict.get('name')}
     response_obj = {"status": obj_json}
