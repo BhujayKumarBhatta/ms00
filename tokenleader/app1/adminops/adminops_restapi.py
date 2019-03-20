1e60ea28cd4b8742b2032b1e59eb0052efaf9a88
project <project@project-PC>
2019-03-20 10:54:33 +0530

    j

--- tokenleader/app1/adminops/adminops_restapi.py
+++ tokenleader/app1/adminops/adminops_restapi.py
@@ -153,7 +153,7 @@ def list_ou(wfc):
 
 @adminops_bp.route('/list/services', methods=['GET'])
 #@enforcer.enforce_access_rule_with_token('tokenleader.adminops.adminops_restapi.list_services')
-def list_services(servicename):
+def list_services():
     services_dict = cf.list_services()
     obj_json = {"name": services_dict.get('name')}
     response_obj = {"status": obj_json}
