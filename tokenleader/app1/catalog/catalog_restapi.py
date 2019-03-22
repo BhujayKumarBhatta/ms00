c47b66ab8540732f3f2934a79997ddde9bd7f083
project <project@project-PC>
2019-03-22 16:02:25 +0530

    h

--- tokenleader/app1/catalog/catalog_restapi.py
+++ tokenleader/app1/catalog/catalog_restapi.py
@@ -18,7 +18,7 @@ enforcer = Enforcer(tlclient)
 #    msg = cf.add_service(servicename, pwd, urlint, urlext, urladmin )     
 #    return msg
 
-@catalog_bp.route('/list/services', methods=['GET'])
+@catalog_bp('/list/services', methods=['GET'])
 def list_services(self):
 #    services_dict = cf.list_service()
 #    obj_json = {"name": services_dict.get('name')}
