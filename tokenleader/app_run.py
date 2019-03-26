6759b4376afd3c44f466729a58fc65fc21a833a3
project <project@project-PC>
2019-03-26 11:45:58 +0530

    catalog_bp responds restapi

--- tokenleader/app_run.py
+++ tokenleader/app_run.py
@@ -2,9 +2,10 @@
 
 from tokenleader import app1
 from tokenleader.app1.configs import configs 
+
 from tokenleader.app1.authentication.token_after_login import token_login_bp
 from tokenleader.app1.adminops.adminops_restapi import adminops_bp
-from tokenleader.app1.catalog.catalog_functions import catalog_bp
+from tokenleader.app1.catalog.catalog_restapi import catalog_bp
 from tokenleader.app1.catalog import models_catalog 
 
 
