c2d4812a627847ed18ec458ae3288afe60a3cc44
project <project@project-PC>
2019-03-26 14:15:09 +0530

    n

--- tokenleader/app1/adminops/adminops_restapi.py
+++ tokenleader/app1/adminops/adminops_restapi.py
@@ -120,7 +120,6 @@ def delete_wfc_restapi(wfcname):
     return jsonify(response_obj)
 
 @adminops_bp.route('/list/org', methods=['GET'])
-@enforcer.enforce_access_rule_with_token('tokenleader.adminops.adminops_restapi.list_org')
 def list_org(wfc):
     org_dict = af.list_org()
     obj_json = {"name": org_dict.get('name')}
