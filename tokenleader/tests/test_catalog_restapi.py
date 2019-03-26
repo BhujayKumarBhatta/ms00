aa17ca63e8b3ba7b15d3dc4af5907710764d70ea
project <project@project-PC>
2019-03-26 17:43:02 +0530

    m

--- tokenleader/tests/test_catalog_restapi.py
+++ tokenleader/tests/test_catalog_restapi.py
@@ -3,12 +3,12 @@ from tokenleader.tests.base_test import  BaseTestCase
 from tokenleader.tests.test_catalog_ops import TestCatalog
 from tokenleader.tests.test_auth import TestToken
 test_token_instance = TestToken()
-tc = TestCatalog()
+t = TestCatalog()
 
 class TestCatalogRestApi(BaseTestCase):
  
     def test_list_services_restapi(self):      
-        u1 = tc.list_services()
+        u1 = t.list_services()
         with self.client:
             response = self.client.get('/list/services/microservice1')
             data = json.loads(response.data.decode())
