75b7087cffaab71ca04f6963e948adb1e448a16c
project <project@project-PC>
2019-03-27 18:57:11 +0530

    m

--- tokenleader/app1/catalog/catalog_restapi.py
+++ tokenleader/app1/catalog/catalog_restapi.py
@@ -12,8 +12,7 @@ tlclient = Client(auth_config)
 enforcer = Enforcer(tlclient)
 
    
-@catalog_bp.route('/list/service/all', methods=['GET'])
-@enforcer.enforce_access_rule_with_token('tokenleader.list_service')
+@catalog_bp.route('/list/service/<name>', methods=['GET'])
 def list_services(wfc):  
     '''
     the function must have a mandatory wfc paramater for applying enforcer decorator
