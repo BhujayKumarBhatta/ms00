c01d6eab67838ae30163029153c7cf030fd68bbb
project <project@project-PC>
2019-03-27 12:33:49 +0530

    l

--- tokenleader/app1/catalog/catalog_restapi.py
+++ tokenleader/app1/catalog/catalog_restapi.py
@@ -19,7 +19,7 @@ def list_services(wfc):
     response_obj = {"status": record_list}
     return jsonify(response_obj)
 
-@catalog_bp.route('/add/service/<name>', methods=['POST'])
+@catalog_bp.route('/add/service/', methods=['POST'])
 @enforcer.enforce_access_rule_with_token('tokenleader.add_service')
 def add_service():   
     record_list = cf.add_service(name, pwd, urlint, urlext, urladmin)
