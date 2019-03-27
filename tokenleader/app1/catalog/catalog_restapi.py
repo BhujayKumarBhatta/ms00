e33f32b2f9a6a1bfda608315575d7bad69c05618
project <project@project-PC>
2019-03-27 12:10:48 +0530

    n

--- tokenleader/app1/catalog/catalog_restapi.py
+++ tokenleader/app1/catalog/catalog_restapi.py
@@ -22,17 +22,16 @@ def list_services(wfc):
 @catalog_bp.route('/add/service', methods=['POST'])
 @enforcer.enforce_access_rule_with_token('tokenleader.add_service')
 def add_service():
-    data_must_contain = ['name', 'pwd', 'urlint', 'urlext', 'urladmin']
+    data_must_contain = ['service_name', 'urlint', 'urlext', 'urladmin']
     for k in data_must_contain:
         if k not in request.json:
             return {"status": " the request must have the following \
             information {}".data_must_contain}
-    name = request.json['name']
-    pwd = request.json['pwd']
+    service_name = request.json['service_name']
     urlint = request.json['urlint']
     urlext = request.json['urlext']
     urladmin = request.json['urladmin']
     #print('i got the name from http argument {}'.format(username))
-    record = cf.add_service(name, pwd, urlint, urlext, urladmin)
+    record = cf.add_service(service_name, urlint, urlext, urladmin)
     response_obj = {"status": record}
     return jsonify(response_obj)
