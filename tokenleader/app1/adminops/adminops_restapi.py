6d44a4f37f4278fe8ecaeeb6784943d7d9414489
project <project@project-PC>
2019-03-27 16:42:53 +0530

    k

--- tokenleader/app1/adminops/adminops_restapi.py
+++ tokenleader/app1/adminops/adminops_restapi.py
@@ -27,7 +27,7 @@ def list_user_byname(username):
     return jsonify(response_obj)
 
 @adminops_bp.route('/list/org', methods=['GET'])
-@enforcer.enforce_access_rule_with_token('tokenleader.adminops.adminops_restapi.list_org')
+#@enforcer.enforce_access_rule_with_token('tokenleader.adminops.adminops_restapi.list_org')
 def list_org(wfc):
     org_dict = af.list_org()
     obj_json = {"name": org_dict.get('name')}
