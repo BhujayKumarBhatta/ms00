379e78268deadc266a543d5ef08acdb05b27d496
project <project@project-PC>
2019-04-03 13:33:09 +0530

    b

--- tokenleader/tests/test_catalog_restapi.py
+++ tokenleader/tests/test_catalog_restapi.py
@@ -8,7 +8,7 @@ t = TestCatalog()
 class TestCatalogRestApi(BaseTestCase):
  
     def test_list_services_restapi(self):      
-        u1 = t.list_services()
+#       u1 = t.list_services()
         with self.client:
             response = self.client.get('/list/service/tokenleader')
             data = json.loads(response.data.decode())
