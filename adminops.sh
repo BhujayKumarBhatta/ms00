b19554f7d53d5357d1a4fcdd2f88359af1de0973
93431 <93431@ITSSBLRINFRL016.ITC.IN>
2019-04-08 10:14:51 +0530

    conflict removed

--- adminops.sh
+++ adminops.sh
@@ -19,30 +19,6 @@ from tokenleader.app1.adminops.admin_cli_parser import main
 if __name__ == '__main__':		
 	main()
 
-=======
-
-# -*- coding: utf-8 -*-
-
-import os
-import sys
-import argparse
-import subprocess
-
-
-possible_topdir = os.path.normpath(os.path.join(os.path.abspath(sys.argv[0]),
-                                                os.pardir,
-                                                os.pardir))
-apppath = (os.path.join(possible_topdir,
-                               'tokenleader',
-                               'tokenleader'))
-
-sys.path.insert(0, apppath)
-
-migration_dir = apppath+'/migrations'
-from tokenleader.app1.adminops.admin_cli_parser import main
-
-if __name__ == '__main__':		
-	main()
 	
 	
 	
