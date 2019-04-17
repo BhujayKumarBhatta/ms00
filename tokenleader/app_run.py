3e81b73f322d89d5c448e09af0c43fcc5de589a5
Prasen <prasen.biswas@itc.in>
2019-04-17 12:06:05 +0530

    app_run.py updated

--- tokenleader/app_run.py
+++ tokenleader/app_run.py
@@ -1,27 +1,18 @@
-# from app1 import flask_app_var
 from tokenleader import app1
-
 from tokenleader.app1.authentication.token_after_login import token_login_bp
 from tokenleader.app1.adminops.adminops_restapi import adminops_bp
+# from app1.catalog.catalog_functions import catalog_bp
 from tokenleader.app1.catalog import models_catalog 
 from tokenleader.app1.configs import prodconfigs
-
 # from prodcon import the prod_conf_ist  and  pass it here , 
 #conf_obj = {"conf": conf}
 #config_list = [conf_obj, c]
-
-
 #bp_list = [token_login_bp, catalog_bp]
 bp_list = [token_login_bp, adminops_bp]
 #print(prodconfigs.prod_conf_list)
 app = app1.create_app(config_map_list= prodconfigs.prod_conf_list,
-bp_list = [token_login_bp, adminops_bp, catalog_bp ]
-
-app = app1.create_app(config_map_list= configs.prod_configs_from_file,
                       blue_print_list=bp_list, )
-
 conf=prodconfigs.conf.yml
-
 host = conf.get('flask_default').get('host_name')
 port = conf.get('flask_default').get('host_port')
 ssl = conf.get('flask_default').get('ssl')
