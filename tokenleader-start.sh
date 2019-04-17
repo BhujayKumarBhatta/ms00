5d7d07a166e3ba898e0c4f78920771b83e3f31e0
Prasen <prasen.biswas@itc.in>
2019-04-17 11:48:38 +0530

    tokenleader-start.sh updated

--- tokenleader-start.sh
+++ tokenleader-start.sh
@@ -6,16 +6,13 @@ import os
 import sys
 import argparse
 
-<<<<<<< HEAD
 
 possible_topdir = os.path.normpath(os.path.join(os.path.abspath(sys.argv[0]),
                                                 os.pardir,
                                                 os.pardir))
-=======
 possible_topdir = os.path.normpath(os.path.join(os.path.abspath(sys.argv[0]),
                                                os.pardir,
                                                os.pardir))
->>>>>>> prasen/tokenleader-tokenrest
 apppath = (os.path.join(possible_topdir,
                                'tokenleaderclient',
                                'tokenleaderclient'))
@@ -26,10 +23,5 @@ from tokenleader.app_run import main
 
 if __name__ == '__main__': 
 	 main()
-<<<<<<< HEAD
-=======
 
-	 
-	 
->>>>>>> prasen/tokenleader-tokenrest
 	 
