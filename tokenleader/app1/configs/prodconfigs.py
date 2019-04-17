13c474580b5cd83bede0be497d47d1885569ad93
Prasen <prasen.biswas@itc.in>
2019-04-17 13:27:37 +0530

    all conflict rectified

--- tokenleader/app1/configs/prodconfigs.py
+++ tokenleader/app1/configs/prodconfigs.py
@@ -65,7 +65,7 @@ with open(public_key_filename, 'r') as f:
 key_attr = {'private_key': private_key, 'public_key': public_key}
 token_settings_map.update(key_attr)
 
-connection_string = 'mysql+pymysql://{0}:{1}@{2}:{3}/{4}'.format(quote_plus(dbs.get('UID')), quote_plus(conf.decrypt_password(dbs.get('db_pwd_key_map'))), dbs.get('Server'), dbs.get('Port'), dbs.get('Database'))
+connection_string = 'mysql+pymysql://{0}:{1}@{2}:{3}/{4}'.format(quote_plus(dbs.get('UID')), conf.decrypt_password(dbs.get('db_pwd_key_map')), dbs.get('Server'), dbs.get('Port'), dbs.get('Database'))
 #converted_safe_uri #use quote_plus to construct the uri
 prod_db_conf = { 'SQLALCHEMY_DATABASE_URI': connection_string, 'SQLALCHEMY_TRACK_MODIFICATIONS': False }
 # pick up values from yml and construct other confs here
