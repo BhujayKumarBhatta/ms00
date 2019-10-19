4d60df5133041c07a53da74c39e9d6f600093f26
Bhujay Kumar Bhatta <bhujay.bhatta@itc.in>
2019-10-19 16:21:28 +0530

    rewriting the authenticator calss

--- tokenleader/app1/authentication/authclass_new.py
+++ tokenleader/app1/authentication/authclass_new.py
@@ -27,35 +27,135 @@ class Authenticator():
         for org type default -  user is retrived from local
     '''
 
-    STATUS = True
-    USERNAME = None
-    PASSWORD = None
-    OTP = None
-    EMAIL = None
-    ORG = 'default'
-    OTP_MODE=None
-    tokenexpiration=30
-    privkey=None
-    OTP_REQUIRED = False
-    BACKEND_CONFIGS = None
-    AUTH_BACKEND = None
-    AUTHENTICATION_STATUS = False
-
-    def __init__(self, request):
-        self._extract_n_validate_data_from_request(request)
-        self._get_auth_backend_from_yml()
+    def __init__(self, request):        
+        self.tokenexpiration=30
+        self.privkey=None
+        self.AUTHENTICATION_STATUS = False
+        self._extract_data_from_request(request)
+        self._get_domain_configs_from_yml()
 #         print(current_app.config.get('token').get('tokenexpiration'))
         if 'tokenexpiration' in current_app.config.get('token'):
             self.tokenexpiration = current_app.config.get('token').get('tokenexpiration')
         self.privkey = current_app.config.get('private_key')
 
+
+    def _extract_data_from_request(self, request):
+        self.USERNAME = None
+        self.PASSWORD = None
+        self.OTP = None
+        self.EMAIL = None
+        self.ORG = 'default'
+        self.OTP_MODE=None
+        regex = re.compile('[@_!#$%^&*()<>?/\|}{~:]')
+        pwdregex = re.compile('[_!#$%^&*()<>?/\|}{~:]')
+        if ('username' in request.json and 
+            len(request.json['username']) <= 40 and 
+            regex.search(request.json['username']) is None and
+            isinstance(request.json['username'], str)):
+            self.USERNAME = request.json['username']
+        if ('password' in request.json and
+            len(request.json['password']) <=15 and 
+            pwdregex.search(request.json['password']) is None and
+            isinstance(request.json['password'], str)):
+            self.PASSWORD = request.json['password']
+        if ('domain' in request.json and
+            len(request.json['domain']) <=10 and 
+            regex.search(request.json['domain']) is None and
+            isinstance(request.json['domain'], str)):
+            self.ORG = request.json['domain']
+            #TODO: change domain key as org
+        if ('otp' in request.json and
+            len(request.json['otp']) == 4 and
+            regex.search(request.json['otp']) is None and
+            isinstance(request.json['otp'], str)):
+            self.OTP = request.json['otp']
+        if ('email' in request.json and
+            len(request.json['email']) <= 40 and 
+            pwdregex.search(request.json['email']) is None and
+            isinstance(request.json['email'], str)):
+            self.EMAIL=request.json['email']
+        result = (self.USERNAME, self.PASSWORD, self.OTP, self.EMAIL, self.ORG )
+        return result
+
+
+    def _get_domain_configs_from_yml(self):
+        ''' doc string '''
+        self.AUTH_BACKEND = None
+        self.BACKEND_CONFIGS = None
+        self.OTP_REQUIRED = True
+        domain_list = current_app.config.get('domains')
+        for domain_dict in domain_list:
+            if self.ORG  in domain_dict:
+                print("found settings for domain: ", self.ORG )
+                domain_settings = domain_dict.get(self.ORG)
+                if domain_settings.get('auth_backend') == 'default':
+                    self.AUTH_BACKEND = 'default'
+                    self.BACKEND_CONFIGS = 'default'
+                    self.OTP_REQUIRED = domain_settings.get('otp_required')
+                elif domain_settings.get('auth_backend') == 'ldap':
+                    self.AUTH_BACKEND = 'ldap'
+                    self.BACKEND_CONFIGS =  self._get_ldap_backend_configs(domain_settings)
+                    self.OTP_REQUIRED = domain_settings.get('otp_required')
+                else:
+                    msg = " unconfigured auth_backend"
+                    print(msg)
+                msg = ("OTP requried status: %s for domain: %s"
+                            %(self.OTP_REQUIRED , self.ORG))
+                break
+            else:
+                msg = ("%s domain has not been configured in  tokenleader_configs"
+                       " by administrator , ask admin to insert this domain in"
+                       " tokenleader_configs" %self.ORG)
+                status = False
+        return_value = {'status': status, 'msg': msg}
+        print(return_value)
+        return return_value
+    
+    def _get_ldap_backend_configs(self, domain_settings):
+        ldap_backend_settings = {'ldap_host': domain_settings.get('ldap_host'),
+           'ldap_port': domain_settings.get('ldap_port'),
+           'ldap_version': domain_settings.get('ldap_version'), 
+           'OU': domain_settings.get('OU'),
+           'O': domain_settings.get('O'),
+           'DC1': domain_settings.get('DC1'),
+           'DC2': domain_settings.get('DC2'),
+           'DC3': domain_settings.get('DC3')}
+        return ldap_backend_settings
+
+    
+
+    def validate_users_domain_name(self):
+        pass
+    
+    def _get_user_dict_from_default_db(self, user=None, email=None):
+        '''use memcahe '''
+        try:
+            if user and not email:  
+                user_from_db  = User.query.filter_by(username=user).first()
+            elif email and not user:
+                user_from_db = User.query.filter_by(email=email).first()
+            elif user and email:
+                user_from_db  = User.query.filter_by(username=user).first()
+            else:
+                user_from_db = None
+            if user_from_db:                
+                user_dict = self._convert_user_to_dict(user_from_db)
+            else:
+                user_dict = ("either user or email is required for login")
+                print(user_dict)
+        except Exception as e:
+            print(e)
+            user_dict = None
+        return user_dict
+        
+
     def get_user_fm_auth_backend(self):
         ''' doc string'''
         if self.AUTH_BACKEND == 'default':
             if self.USERNAME:
-                user_fm_backend = self._get_user_info_from_default_db(user=self.USERNAME)
+                user_fm_backend = self._get_user_dict_from_default_db(user=self.USERNAME)
             elif self.EMAIL:
-                user_fm_backend = self._get_user_info_from_default_db(email=self.EMAIL)
+                user_fm_backend = self._get_user_dict_from_default_db(email=self.EMAIL)
             else:
                 pass
         elif self.AUTH_BACKEND == 'ldap':
@@ -107,16 +207,9 @@ class Authenticator():
             service_catalog[s.name]=s.to_dict()
         return service_catalog
             
-    def _get_user_info_from_default_db(self, user=None, email=None):
-        '''use memcahe '''
-        if user and not email:  
-            user_from_db  = User.query.filter_by(username=user).first()
-        elif email and not user:
-            user_from_db = User.query.filter_by(email=email).first()
-        elif user and email:
-            user_from_db  = User.query.filter_by(username=user).first()
-        else:
-            result = ("either user or email is required for login")
+    
+            
+        
         #Once user object is available
         #We check if OTP is available and return user
         #else We check passowrd return user   
@@ -241,88 +334,8 @@ class Authenticator():
         	self.AUTHENTICATION_STATUS = False
         	return False
 
-    def _extract_n_validate_data_from_request(self, request):
-         regex = re.compile('[@_!#$%^&*()<>?/\|}{~:]')
-         pwdregex = re.compile('[_!#$%^&*()<>?/\|}{~:]')
-         if 'username' in request.json:
-            if len(request.json['username']) <= 40 and regex.search(request.json['username']) is None \
-         and isinstance(request.json['username'], str):
-                self.USERNAME = request.json['username']
-                #print(self.USERNAME)
-            else:
-                self.STATUS = False
-         if 'password' in request.json:
-            if len(request.json['password']) <=15 and pwdregex.search(request.json['password']) is None \
-         and isinstance(request.json['password'], str):
-                self.PASSWORD = request.json['password']
-                #print(self.PASSWORD)
-            else:
-                self.STATUS = False
-         if 'domain' in request.json:
-            if len(request.json['domain']) <=10 and regex.search(request.json['domain']) is None \
-         and isinstance(request.json['domain'], str):
-                self.ORG = request.json['domain']   #TODO: change domain key as org
-                #print(self.ORG)
-            else:
-                self.STATUS = False
-         if 'otp' in request.json:
-            if len(request.json['otp']) == 4 and regex.search(request.json['otp']) is None \
-         and isinstance(request.json['otp'], str):
-                self.OTP = request.json['otp']
-                #print(self.OTP)
-            else:
-                self.STATUS = False
-         if 'email' in request.json:
-            if len(request.json['email']) <= 40 and pwdregex.search(request.json['email']) is None \
-         and isinstance(request.json['email'], str):
-                self.EMAIL=request.json['email']
-                #print(self.EMAIL)
-            else:
-                self.STATUS = False
 
-    def _get_auth_backend_from_yml(self):
-        ''' doc string '''
-        # backend_configs = 'default'
-        domain_list = current_app.config.get('domains')
-#         print("list of domains from yml:", domain_list)
-#         print("self.ORG:", self.ORG)
-        for domain_dict in domain_list:
-            if self.ORG  in domain_dict:
-                print("found settings for domain: ", self.ORG )
-                domain_settings = domain_dict.get(self.ORG)
-                if domain_settings.get('auth_backend') == 'default':
-                    self.AUTH_BACKEND = 'default'
-                    self.BACKEND_CONFIGS = 'default'
-                    self.OTP_REQUIRED = domain_settings.get('otp_required')                    
-                elif domain_settings.get('auth_backend') == 'ldap':
-                    self.AUTH_BACKEND = 'ldap'
-                    self.BACKEND_CONFIGS =  {'ldap_host': domain_settings.get('ldap_host'),
-                                   'ldap_port': domain_settings.get('ldap_port'),
-                                   'ldap_version': domain_settings.get('ldap_version'), 
-                                   'OU': domain_settings.get('OU'),
-                                   'O': domain_settings.get('O'),
-                                   'DC1': domain_settings.get('DC1'),
-                                   'DC2': domain_settings.get('DC2'),
-                                   'DC3': domain_settings.get('DC3')}
-                    self.OTP_REQUIRED = domain_settings.get('otp_required')
-                elif domain_settings.get('auth_backend') == 'active_directory':
-                    pass
-                elif domain_settings.get('auth_backend') == 'sqldb':
-                    pass
-                else:
-                    msg = " unconfigured auth_backend"
-                    status = False
-                status = True
-                msg = ("OTP requried status: %s for domain: %s"
-                            %(self.OTP_REQUIRED , self.ORG))
-                break
-            else:
-                msg = ("%s domain has not been configured in  tokenleader_configs"
-                       " by administrator" %self.ORG)
-                status = False
-        return_value = {'status': status, 'msg': msg}
-        print(return_value)
-        return return_value
+    
 
 
 
