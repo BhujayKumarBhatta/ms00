c74e88d7e620a6d40ff056af2ec8f398c94c1d17
Bhujay Kumar Bhatta <bhujay.bhatta@itc.in>
2019-10-26 10:22:13 +0530

    otp_mode view option

--- tokenleader/app1/authentication/otp.py
+++ tokenleader/app1/authentication/otp.py
@@ -55,7 +55,7 @@ class Otpmanager():
             kp.notify_kafka(conf, self._get_wfc(), 
                                 "topic_tokenleader", kafka_response)
 
-            if self.OTP_MODE == "display":
+            if self.OTP_MODE == "view":
                 message =  ('OTP: %s SENT to %s with %s seconds validity'
                             %(self.OTP_MODE, str(otpvalidtime), num))
             message =  ('OTP SENT to %s with %s seconds validity'
@@ -116,6 +116,7 @@ class Otpmanager():
         except Exception as e:
             print (e)
             status = {"status": "OTPGeneratonFiled", "message": e}
+            db.session.rollback()        
         return status
     
     
